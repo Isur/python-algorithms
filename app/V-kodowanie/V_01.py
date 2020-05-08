a70200aef188073f0ee5b2ea9096dc1ca096ee10
Isur <bednarczykartur96@gmail.com>
2020-05-08 21:20:18 +0200

    Ceasar with any shift

--- app/V-kodowanie/V_01.py
+++ app/V-kodowanie/V_01.py
@@ -1,3 +1,32 @@
 """
     Cezar z dowolnym przesunięciem.
 """
+
+
+def caesar(text, shift):
+    alphabet = ""
+    code = ""
+
+    for a in range(65, 91):  # 65 - A, 90 - Z
+        alphabet += chr(a)
+
+    size = len(alphabet)
+
+    for char in text:
+        if char.isupper():
+            pos = (alphabet.index(char) + shift) % size
+            code += alphabet[pos]
+        elif char.islower():
+            pos = (alphabet.index(char.upper()) + shift) % size
+            code += alphabet[pos].lower()
+        else:
+            code += char
+
+    return code
+
+
+str = "THIS IS some SECRET INFORMATION :)"
+shift = 13
+print(f"To code: {str}")
+coded = caesar(str, shift)
+print(f"Coded: {coded}")
